f60cb8d8c57c5b07f43726c97d8f8be4ef8014ad
abdullah diaa <abdullah.diaa@gmail.com>
2021-05-14 02:17:33 +0300

    Add inital code && testing for sending notifications

--- gowebpusher.go
+++ gowebpusher.go
@@ -1,2 +1,40 @@
 //Package gowebpusher helps sending push notifications to web browsers
 package gowebpusher
+
+//Sender instance
+type Sender struct {
+	PushSubscriptions []PushSubscription
+}
+
+//PushSubscription interface of the Push API provides a subscription's URL endpoint.
+type PushSubscription struct {
+	Endpoint       string
+	SubscriptionID string
+	key            PushSubscriptionKey
+}
+
+//PushSubscriptionKey represents a client public key, which can then be sent to a server and used in encrypting push message data.
+// P256dh: An Elliptic curve Diffie–Hellman public key on the P-256 curve (that is, the NIST secp256r1 elliptic curve).  The resulting key is an uncompressed point in ANSI X9.62 format.
+// Auth: An authentication secret, as described in Message Encryption for Web Push.
+type PushSubscriptionKey struct {
+	P256dh string
+	Auth   string
+}
+
+//NewSender will initialize an instance of sender
+func NewSender() *Sender {
+	s := &Sender{}
+	s.Initialize()
+	return s
+}
+
+//Initialize will set the default values of the sender instance
+func (s *Sender) Initialize() {
+	s.PushSubscriptions = make([]PushSubscription, 0, 1000)
+}
+
+//Send will deliver the notification to all subscriptions
+func (s *Sender) Send() int {
+	//Testing return
+	return len(s.PushSubscriptions)
+}
--- gowebpusher_test.go
+++ gowebpusher_test.go
@@ -1,5 +1,27 @@
 package gowebpusher
 
-func init() {
+import "testing"
 
+func TestSend(t *testing.T) {
+	//Create sender instance
+	s := NewSender()
+	PushSubscriptions := []PushSubscription{
+		{
+			Endpoint:       "ENDPOINT",
+			SubscriptionID: "ID",
+			key: PushSubscriptionKey{
+				P256dh: "P256dh",
+				Auth:   "AUTH KEY",
+			},
+		},
+	}
+	s.PushSubscriptions = PushSubscriptions
+	res := s.Send()
+	assertEqual(t, res, 1)
+}
+
+func assertEqual(t *testing.T, a interface{}, b interface{}) {
+	if a != b {
+		t.Fatalf("%s != %s", a, b)
+	}
 }
