c33fbc3fe00e04975e3d4ca495b39b76dcfbbf62
abdullah diaa <abdullah.diaa@gmail.com>
2021-06-13 14:35:17 +0300

    test VAPID keys length

--- gowebpusher_test.go
+++ gowebpusher_test.go
@@ -42,6 +42,17 @@ func TestKeys(t *testing.T) {
 		}
 		t.Logf("\t%s\tPublic key and Private key generated successfully", succeed)
 
+		//Validate keys length
+		if len(pubKey) != 87 {
+			t.Fatalf("\t%s\tInvalid Public VAPID key", failed)
+		}
+		t.Logf("\t%s\tPrivate key length is valid", succeed)
+
+		if len(privKey) != 43 {
+			t.Fatalf("\t%s\tInvalid Private VAPID key", failed)
+		}
+		t.Logf("\t%s\tPublic key length is valid", succeed)
+
 		_, err = base64.RawURLEncoding.DecodeString(privKey)
 		if err != nil {
 			t.Fatalf("\t%s\tInvalid Private key: %s", failed, err.Error())
